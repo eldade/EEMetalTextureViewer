5382b8a8439a8691bab01b6ed151233e2325005c
eldoogy <ldoogy@gmail.com>
2016-09-21 14:25:40 -0700

    Initial Commit

---
+++ eldade_metal_tests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eldade_metal_tests
+//
+//  Created by Eldad Eilam on 9/21/16.
+//  Copyright © 2016 Eldad Eilam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
